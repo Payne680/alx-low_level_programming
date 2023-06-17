1254fcb7dcc2a5bf0b71971cdd83cb6065f18a58
Penn Junior <pennjunior680@gmail.com>
2023-06-17 05:53:42 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+  *main - Entry point
+  *Description - "Print alphabets in lowercase"
+  *Return: Will be 0 (success)
+  */
+int main(void)
+{
+	int n = 97;
+
+	for (int n; n <= 122; n++)
+
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
