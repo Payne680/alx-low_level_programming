6c68c639cbfa9d0226a975f36e3038407c9eff6a
Penn Junior <pennjunior680@gmail.com>
2023-06-20 18:27:34 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+    *main - Entry point
+    *Description "Will print _putchar to stdout"
+    *Return: Will be 0 (success)
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+
