ad53442d8c842b3690e20bbb535507b5dc479f0f
PENN JR <pennjunior680@gmail.com>
2023-08-31 20:59:06 +0100

    Working on the 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,13 +9,13 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit_val;
+	int numm;
 
 	if (index > 63)
 		return (-1);
 
-	bit_val = (n >> index) & 1;
+	num = (n >> index) & 1;
 
-	return (bit_val);
+	return (num);
 }
 
