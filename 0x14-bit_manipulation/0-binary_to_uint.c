c2ecd434abe5631defa8dc719a0532e83cd8c8e9
PENN JR <pennjunior680@gmail.com>
2023-08-31 20:30:53 +0100

    Doing some corrections

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,20 +6,20 @@
  *
  * Return: the converted number
  */
-unsigned int binary_to_uint(const char *b) {
-  unsigned int dec_val = 0;
-  int i;
+unsigned int binary_to_uint(const char *b)
+{
+        int x;
+        unsigned int num = 0;
 
-  if (b == NULL) {
-    return 0;
-  }
+        if (!b)
+                return (0);
 
-  for (i = strlen(b) - 1; i >= 0; i--) {
-    if (b[i] != '0' && b[i] != '1') {
-      return 0;
-    }
-    dec_val |= (b[i] - '0') << i;
-  }
+        for (x = 0; b[x]; x++)
+        {
+                if (b[x] < '0' || b[x] > '1')
+                        return (0);
+                num = 2 * num + (b[x] - '0');
+        }
 
-  return dec_val;
+        return (num);
 }
