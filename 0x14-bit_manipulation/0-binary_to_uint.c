34269dc9ad9c9fa403349e25a399e580d342c058
PENN JR <pennjunior680@gmail.com>
2023-08-31 20:21:18 +0100

    Working on Binary to unit task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,10 +7,10 @@
  * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b) {
-  int i;
   unsigned int dec_val = 0;
+  int i;
 
-  if (!b) {
+  if (b == NULL) {
     return 0;
   }
 
@@ -18,9 +18,8 @@ unsigned int binary_to_uint(const char *b) {
     if (b[i] != '0' && b[i] != '1') {
       return 0;
     }
-    dec_val = dec_val * 2 + (b[i] - '0');
+    dec_val |= (b[i] - '0') << i;
   }
 
   return dec_val;
 }
-
